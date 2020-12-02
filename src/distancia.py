37816912812d520df69d5da00bcaa0acedab7356
Luiz Paulino <lp.luizpaulino@gmail.com>
2020-12-02 13:10:00 -0300

    Refatoração para as funções ficarem mais clean

--- src/distancia.py
+++ src/distancia.py
@@ -1,44 +1,86 @@
 import math
 
-def calcula_distancia_lojas(posicao_cliente: list[int], lojas: list[list[int]], plano: list[int]):
-  distancias = {}
 
-  for loja in lojas:
-
+def valida_dados_entrada(plano: list[int], posicao_cliente: list[int]):
     if (plano[0] < 0) or (plano[0] > 1000):
-      raise Exception('Plano M inválido')
+        raise Exception('Plano M inválido')
 
     if (plano[1] < 0) or (plano[1] > 1000):
-      raise Exception('Plano N inválido')
-
-
-    loja_x = loja[0]
-    loja_y = loja[1]
+        raise Exception('Plano N inválido')
 
     cliente_x = posicao_cliente[0]
     cliente_y = posicao_cliente[1]
 
     if (cliente_x < 0) or (cliente_x > plano[0]):
-      raise Exception('Posição X do cliente inválida')
-    
+        raise Exception('Posição X do cliente inválida')
+
     if (cliente_y < 0) or (cliente_y > plano[1]):
-      raise Exception('Posição Y do cliente inválida')
+        raise Exception('Posição Y do cliente inválida')
 
-    distancia = ((loja_x - cliente_x) ** 2) + ((loja_y - cliente_y) ** 2) 
-    distancia_ab = math.sqrt(distancia)
 
-    distancias.update({
-      distancia_ab: loja
-    })
+def calcula_distancia_lojas(posicao_cliente: list[int], lojas: list[list[int]]):
+    distancias = {}
 
-  todas_lojas = list(distancias.keys())
-  todas_lojas.sort()
-  todas_lojas = todas_lojas[0:3]
+    cliente_x = posicao_cliente[0]
+    cliente_y = posicao_cliente[1]
 
-  lojas_mais_proximas = []
+    for loja in lojas:
 
-  for loja in todas_lojas:
-    lojas_mais_proximas.append(distancias[loja])
+        loja_x = loja[0]
+        loja_y = loja[1]
 
-  return lojas_mais_proximas
+        distancia = ((loja_x - cliente_x) ** 2) + ((loja_y - cliente_y) ** 2)
+        distancia_ab = math.sqrt(distancia)
 
+        distancias.update({
+            distancia_ab: loja
+        })
+
+    return distancias
+
+
+def ordenacao_insercao(distancias: list[int]):
+    for i in range(1, len(distancias)):
+
+        distancia_atual = distancias[i]
+        j = i-1
+        while j >= 0 and distancia_atual < distancias[j]:
+            distancias[j+1] = distancias[j]
+            j -= 1
+        distancias[j+1] = distancia_atual
+
+    return distancias
+
+
+def tres_lojas_mais_proximas(todas_lojas: dict):
+    distancia_todas_lojas = list(todas_lojas.keys())
+
+    lojas_ordenadas = ordenacao_insercao(distancia_todas_lojas)
+    tres_lojas_mais_proximas = []
+
+    for loja in lojas_ordenadas[0:3]:
+        tres_lojas_mais_proximas.append(todas_lojas[loja])
+
+    return tres_lojas_mais_proximas
+
+
+def lojas_mais_proximas(posicao_cliente: list[int], lojas: list[list[int]], plano: list[int]):
+
+    valida_dados_entrada(plano=plano, posicao_cliente=posicao_cliente)
+
+    distancias = calcula_distancia_lojas(
+        posicao_cliente=posicao_cliente, lojas=lojas)
+
+    lojas = tres_lojas_mais_proximas(distancias)
+
+    return lojas
+
+
+if __name__ == "__main__":
+    posicao_cliente = [20, 32]
+    lojas = [[40, 88], [99, 93], [99, 97], [18, 56], [99, 2], [99, 99]]
+    plano = [100, 100]
+
+    resultado = lojas_mais_proximas(posicao_cliente, lojas, plano)
+
+    print(resultado)
--- src/distancia_test.py
+++ src/distancia_test.py
@@ -1,36 +1,69 @@
 import unittest
 
-from distancia import calcula_distancia_lojas
+from distancia import calcula_distancia_lojas, valida_dados_entrada, lojas_mais_proximas, ordenacao_insercao, tres_lojas_mais_proximas
 
-class TestLojaMaisProxima(unittest.TestCase):
 
-    def test_lojas_mais_proximas_com_quatro_lojas(self):
-        
-        posicao_cliente = [20, 32]
-        lojas = [[40, 88], [18, 56], [99, 2], [99,99]]
-        plano = [100, 100]
+class TestValidaDadosEntrada(unittest.TestCase):
 
-        resultado = calcula_distancia_lojas(posicao_cliente, lojas, plano)
+    def setUp(self):
+        self.posicao_cliente = [20, 101]
+        self.plano = [100000, 100]
 
-        self.assertEqual([[18, 56], [40, 88], [99, 2]], resultado)
-
-    def test_lojas_mais_proximas_com_uma_loja(self):
-        
-        posicao_cliente = [20, 32]
-        lojas = [[40, 88]]
-        plano = [100, 100]
-
-        resultado = calcula_distancia_lojas(posicao_cliente, lojas, plano)
-
-        self.assertEqual([[40, 88]], resultado)
-
-    def test_lojas_mais_proximas_com_posicao_cliente_invalida(self):
+    def test_valida_dados_entrada_com_posicao_cliente_invalida(self):
         with self.assertRaises(Exception):
-            posicao_cliente = [20, 101]
-            lojas = [[40, 88]]
-            plano = [100, 100]
 
-            resultado = calcula_distancia_lojas(posicao_cliente, lojas, plano)
+            resultado = valida_dados_entrada(
+                plano=self.plano, posicao_cliente=self.posicao_cliente)
+
+    def test_valida_dados_entrada_com_plano_invalido(self):
+        with self.assertRaises(Exception):
+
+            resultado = valida_dados_entrada(
+                plano=plano, posicao_cliente=posicao_cliente)
+
+
+class TestCalculaDistanciaLojas(unittest.TestCase):
+
+    def setUp(self):
+        self.lojas = [[40, 88], [99, 93], [
+            99, 97], [18, 56], [99, 2], [99, 99]]
+        self.posicao_cliente = [20, 32]
+        self.distancias = calcula_distancia_lojas(
+            posicao_cliente=self.posicao_cliente, lojas=self.lojas)
+        self.lojas_proximas = [[18, 56], [40, 88], [99, 2]]
+        self.plano = [100, 100]
+
+    def test_calcula_distancia_lojas(self):
+
+        resultado_esperado = {59.464274989274024: [40, 88], 99.80981915623332: [99, 93], 102.30347012687302: [
+            99, 97], 24.08318915758459: [18, 56], 84.50443775329198: [99, 2], 103.58571330062848: [99, 99]}
+
+        resultado = calcula_distancia_lojas(
+            lojas=self.lojas, posicao_cliente=self.posicao_cliente)
+
+        self.assertEqual(resultado, resultado_esperado)
+
+    def test_ordenacao_insercao(self):
+        lista_desordenada = [73, 20, 19, 3, 99, 5, 1]
+
+        resultado_esperado = [1, 3, 5, 19, 20, 73, 99]
+
+        resultado = ordenacao_insercao(lista_desordenada)
+
+        self.assertEqual(resultado, resultado_esperado)
+
+    def test_tres_lojas_mais_proximas(self):
+
+        resultado = tres_lojas_mais_proximas(self.distancias)
+
+        self.assertEqual(resultado, self.lojas_proximas)
+
+    def test_lojas_mais_proximas(self):
+        resultado = lojas_mais_proximas(
+            self.posicao_cliente, self.lojas, self.plano)
+
+        self.assertEqual(resultado, self.lojas_proximas)
+
 
 if __name__ == '__main__':
     unittest.main()
